faf26ac9303edc55e23e4c486433454158a086f8
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2021-01-11 12:47:15 +0100

    leading whitespaces removed

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ class InvalidFilenameCorrecter:
         self.path = path
         self.invalid_dir_count = 0
         self.invalid_file_count = 0
-        self.files_in_invalid_dir = 0
-        self.invalid_char_set = ('"', '*', ':', '<', '>', '&', '?', '|')
+        self.invalid_char_set = ('"', '*', ':', '<', '>', '&', '?', '|', 'ä', 'ü', 'ö')
 
     def invalid_characters(self, filename):
         """ This method returns True if invalid characters were detected """
@@ -27,19 +26,23 @@ class InvalidFilenameCorrecter:
 
         # Iterate over all directories
         for directories in full_directory:
+
+            # increment counters
+            self.invalid_dir_count += 1
+
             filepath = directories[0]
 
+            # Check for invalid characters
             if self.invalid_characters(filepath):
                 updated_filepath = self.replace_invalid_characters(filepath)
 
-                # Print updated filepaths
+                # Print updated filepath
                 print('Invalid directory: ', filepath)
                 print('Updated directory', updated_filepath)
 
-                # increment counters
-                self.invalid_dir_count += 1
-                self.files_in_invalid_dir += len(directories[2])
+                # Todo: check whitespaces
 
+                # Rename directory without invalid characters
                 try:
                     os.rename(filepath, updated_filepath)
                 except Exception:
@@ -54,7 +57,7 @@ class InvalidFilenameCorrecter:
             # Iterate over all files in currently selected directory
             for filename in directories[2]:
 
-                if self.invalid_characters(filename):
+                if self.invalid_characters(filename) or filename[0] == ' ':
 
                     # increment counters
                     self.invalid_file_count += 1
@@ -74,19 +77,16 @@ class InvalidFilenameCorrecter:
                     except Exception:
                         logging.warning('File could not be renamed')
 
-
-
     def get_result(self):
         print(self.invalid_dir_count, 'invalid directory names have been found.')
         print(self.invalid_file_count, 'invalid filenames have been found.')
-        print(self.files_in_invalid_dir, 'files in invalid directories that might not be synced.')
-        print('Total: ', self.invalid_file_count + self.invalid_dir_count + self.files_in_invalid_dir)
+        print('Total: ', self.invalid_file_count + self.invalid_dir_count)
 
     def replace_invalid_characters(self, string):
         for c in self.invalid_char_set:
             if c in string:
                 string = string.replace(c, '_')
-        return string
+        return string.strip()
 
 
 # Press the green button in the gutter to run the script.
